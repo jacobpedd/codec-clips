47b4489bf0d185533c6fd894b21b54c9cc451dce
Jacob Peddicord <jpmail17@gmail.com>
2024-07-03 15:29:52 -0700

    need session auth for UI browsing

--- codec/settings.py
+++ codec/settings.py
@@ -139,6 +139,7 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 25,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
