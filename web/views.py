74994a6b68920f4eaa5c5362816e053cb306785e
Jacob Peddicord <jpmail17@gmail.com>
2024-07-18 15:52:34 -0600

    New queue with cold start mode

--- web/views.py
+++ web/views.py
@@ -16,7 +16,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.db import IntegrityError, connection
-from django.db.models import Q
+from django.db.models import Q, F, Case, When, Value, FloatField, Subquery, OuterRef
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
@@ -29,9 +29,11 @@ from django.urls import reverse
 from web import serializers
 from web.models import (
     Clip,
+    ClipUserScore,
     ClipUserView,
     FeedUserInterest,
     Feed,
+    FeedUserScore,
 )
 import resend
 
@@ -40,48 +42,104 @@ class QueueViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = serializers.ClipSerializer
     permission_classes = [IsAuthenticated]
 
+    # TODO: Time decay
     def get_queryset(self):
         user = self.request.user
-        one_week_ago = timezone.now() - timedelta(days=7)
 
         # Get exclude_clip_ids from query parameters
-        exclude_clip_ids = self.request.query_params.getlist("exclude_clip_ids", [])
-
         # If only one exclude_clip_ids param is provided as a comma-separated string, split it
+        exclude_clip_ids = self.request.query_params.getlist("exclude_clip_ids", [])
         if len(exclude_clip_ids) == 1 and "," in exclude_clip_ids[0]:
             exclude_clip_ids = exclude_clip_ids[0].split(",")
 
-        base_queryset = (
-            Clip.objects.filter(
-                created_at__gte=one_week_ago,
-                user_scores__user=user,
-                feed_item__feed__is_english=True,
-            )
-            .exclude(user_views__user=user)
-            .exclude(id__in=exclude_clip_ids)
-            .select_related("feed_item__feed")
-            .prefetch_related("user_scores")
+        # Subquery to get the latest clip ID for each feed
+        latest_clip_subquery = (
+            Clip.objects.filter(feed_item__feed=OuterRef("feed_item__feed"))
+            .order_by("-id")
+            .values("id")[:1]
         )
 
-        # Get top clips
-        top_clips = list(
-            base_queryset.order_by("-user_scores__score", "-created_at").distinct()[:9]
-        )
-        if len(top_clips) == 0:
-            return []
+        # Check if we should do cold start or use ClipUserScores
+        ranked_clips = []
+        if ClipUserScore.objects.filter(user=user).count() < 10:
+            # Cold start because the user doesn't have enough ClipUserScores
+            print("Using cold start")
+            ranked_clips = (
+                Clip.objects.filter(
+                    id=Subquery(latest_clip_subquery),  # Latest clip for each feed
+                    feed_item__feed__is_english=True,  # Feed is English
+                    feed_item__feed__user_scores__user=user,  # Feed has a user score
+                )
+                .exclude(id__in=exclude_clip_ids)  # exclude_clip_ids
+                .exclude(user_views__user=user)  # exclude viewed clips
+                .annotate(
+                    feed_score=F("feed_item__feed__user_scores__score"),
+                    feed_popularity=F("feed_item__feed__popularity_percentile"),
+                    combined_score=Case(
+                        When(
+                            feed_score__isnull=False,
+                            # Combine feed score and popularity
+                            then=F("feed_score") * 0.8 + F("feed_popularity") * 0.2,
+                        ),
+                        default=Value(0),
+                        output_field=FloatField(),
+                    ),
+                )
+                .order_by("-combined_score")[:9]
+            )
+        else:
+            print("Using clip scores")
+            ranked_clips = (
+                Clip.objects.filter(
+                    id=Subquery(latest_clip_subquery),  # Latest clip for each feed
+                    feed_item__feed__is_english=True,  # Feed is English
+                    feed_item__feed__user_scores__user=user,  # Feed has a user score
+                    user_scores__user=user,  # Clip has a user score
+                )
+                .exclude(id__in=exclude_clip_ids)  # exclude_clip_ids
+                .exclude(user_views__user=user)  # exclude viewed clips
+                .annotate(
+                    feed_score=F("feed_item__feed__user_scores__score"),
+                    feed_popularity=F("feed_item__feed__popularity_percentile"),
+                    clip_score=F("user_scores__score"),  # Include the ClipUserScore
+                    combined_score=Case(
+                        # Combine feed score, popularity, and clip score
+                        When(
+                            feed_score__isnull=False,
+                            then=F("feed_score") * 0.4
+                            + F("feed_popularity") * 0.2
+                            + F("clip_score") * 0.4,
+                        ),
+                        default=Value(0),
+                        output_field=FloatField(),
+                    ),
+                )
+                .order_by("-combined_score")[:9]
+            )
 
         # Get a random clip
+        one_week_ago = timezone.now() - timedelta(days=7)
         random_clip = (
-            base_queryset.filter(user_scores__score__lt=0.5).order_by("?").first()
+            Clip.objects.filter(
+                created_at__gte=one_week_ago,  # Created in the last week
+                user_scores__user=user,  # User has a user score
+                feed_item__feed__is_english=True,  # Feed is English
+            )
+            .exclude(user_views__user=user)  # exclude viewed clips
+            .exclude(id__in=exclude_clip_ids)  # exclude_clip_ids
+            .filter(user_scores__score__lt=0.5)  # User score is less than 0.5
+            .order_by("?")  # Random order
+            .first()
         )
         if random_clip is None:
-            return top_clips
+            return ranked_clips
 
         # Insert the random clip at a random position in the top clips
-        random_index = random.randint(0, len(top_clips))
-        top_clips.insert(random_index, random_clip)
+        ranked_clips = list(ranked_clips)
+        random_index = random.randint(0, len(ranked_clips))
+        ranked_clips.insert(random_index, random_clip)
 
-        return top_clips
+        return ranked_clips
 
 
 class FeedViewSet(viewsets.ReadOnlyModelViewSet):
